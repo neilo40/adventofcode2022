791aab629a5053bc7987885d2c793b37851fbccc
neil040 <neil@neiloconnor.com>
2022-12-02 10:20:06 +0000

    day2

--- day2/main.go
+++ day2/main.go
@@ -4,7 +4,26 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
+
+var scoring = map[string]map[string]int{
+	"A": { //rock
+		"X": 1 + 3, // rock - draw
+		"Y": 2 + 6, // paper - win
+		"Z": 3,     // scissors - lose
+	},
+	"B": { // paper
+		"X": 1,
+		"Y": 2 + 3,
+		"Z": 3 + 6,
+	},
+	"C": { // scissors
+		"X": 1 + 6,
+		"Y": 2,
+		"Z": 3 + 3,
+	},
+}
 
 func main() {
 	part1()
@@ -20,11 +39,47 @@ func part1() {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
+	score := 0
 	for scanner.Scan() {
-		//t := scanner.Text()
-
+		t := scanner.Text()
+		plays := strings.Fields(t)
+		score += scoring[plays[0]][plays[1]]
 	}
+	log.Printf("Score: %d\n", score)
+}
+
+var scoring2 = map[string]map[string]int{
+	"A": { //rock
+		"X": 3,
+		"Y": 1 + 3,
+		"Z": 2 + 6,
+	},
+	"B": { // paper
+		"X": 1,
+		"Y": 2 + 3,
+		"Z": 3 + 6,
+	},
+	"C": { // scissors
+		"X": 2,
+		"Y": 3 + 3,
+		"Z": 1 + 6,
+	},
 }
 
 func part2() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Couldn't open input")
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	score := 0
+	for scanner.Scan() {
+		t := scanner.Text()
+		plays := strings.Fields(t)
+		score += scoring2[plays[0]][plays[1]]
+	}
+	log.Printf("Score (part 2): %d\n", score)
 }
