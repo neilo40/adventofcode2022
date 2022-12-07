b0072bc98a6be6f70ad62eb753cbc7ee37341267
neilo40 <neil@neiloconnor.com>
2022-12-07 11:14:36 +0000

    day6

--- day6/main.go
+++ day6/main.go
@@ -20,11 +20,53 @@ func part1() {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
-	for scanner.Scan() {
-		//t := scanner.Text()
-
+	scanner.Scan()
+	t := scanner.Text()
+	for i := 0; i < len(t)-4; i++ {
+		freq := make(map[byte]bool)
+		dupeFound := false
+		for j := 0; j < 4; j++ {
+			_, ok := freq[t[i+j]]
+			if ok {
+				dupeFound = true
+				break
+			} else {
+				freq[t[i+j]] = true
+			}
+		}
+		if !dupeFound {
+			log.Printf("Start of packet marker at %d\n", i+4)
+			break
+		}
 	}
 }
 
 func part2() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Couldn't open input")
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	scanner.Scan()
+	t := scanner.Text()
+	for i := 0; i < len(t)-14; i++ {
+		freq := make(map[byte]bool)
+		dupeFound := false
+		for j := 0; j < 14; j++ {
+			_, ok := freq[t[i+j]]
+			if ok {
+				dupeFound = true
+				break
+			} else {
+				freq[t[i+j]] = true
+			}
+		}
+		if !dupeFound {
+			log.Printf("Start of message marker at %d\n", i+14)
+			break
+		}
+	}
 }
